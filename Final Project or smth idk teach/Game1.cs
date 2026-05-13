cf5fe1f5b685caaedd25273ddf4a36f4b2dc5245
Cameron Link <362704579@GoTVDSB.ca>
2026-05-13 13:19:27 -0400

    Added Stufff

--- Final Project or smth idk teach/Game1.cs
+++ Final Project or smth idk teach/Game1.cs
@@ -6,6 +6,7 @@ enum Screen
 {
     Title,
     Play,
+    TowerPick,
     Easy,
     Normal,
     Hard
@@ -19,16 +20,19 @@ namespace Final_Project_or_smth_idk_teach
         Screen screen;
         MouseState mouseState, prevMouseState;
         private Song Menu;
-        Texture2D temp, bg, titleScreen, map, playButton, easyButton, normalButton, hardButton, scout;
-        Rectangle playRec, easyRec, normalRec, hardRec, window;
+        Texture2D temp, bg, titleScreen, map, playButton, easyButton, normalButton, hardButton, scout, inventory;
+        Rectangle playRec, easyRec, normalRec, hardRec, window, scoutRec, inventoryRec;
         float opacity = 0f;
         int sizeChange = 2, coordChange = 1;
         int smalldown = 2, smallcoord = 1;
-        Vector2 position = new Vector2(100, 200);
+        Vector2 position = new Vector2(200, 300);
         float scale = 0.4f;
         const float NormalScale = 0.4f;
-        const float HoverScale = 0.6f;
-        const float LerpSpeed = 0.15f; 
+        const float HoverScale = 0.5f;
+        const float LerpSpeed = 0.15f;
+        Button btnEasy;
+        Button btnNormal;
+        Button btnHard;
         public Game1()  
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -42,7 +46,8 @@ namespace Final_Project_or_smth_idk_teach
             // Rectangles Below
             window = new Rectangle(0, 0, 1000, 800);
             playRec = new Rectangle(400, 400, 200, 100);
-            easyRec = new Rectangle(100, 200, 200, 200);
+            inventoryRec = new Rectangle(400, 400, 200, 100);
+            easyRec = new Rectangle(10, 10, 10, 10);
             normalRec = new Rectangle(400, 200, 200, 200);
             hardRec = new Rectangle(700, 200, 200, 200);
             _graphics.PreferredBackBufferWidth = window.Width;  // set this value to the desired width of your window
@@ -54,71 +59,60 @@ namespace Final_Project_or_smth_idk_teach
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+           
             // Textures Below
             playButton = Content.Load<Texture2D>("PlayButton");
+            inventory = Content.Load<Texture2D>("inventoryTemp");
             easyButton = Content.Load<Texture2D>("easyMode");
             normalButton = Content.Load<Texture2D>("moltenMode");
             hardButton = Content.Load<Texture2D>("fallenMode");
             map = Content.Load<Texture2D>("crossroadsUnfinished2");
             temp = Content.Load<Texture2D>("tempImage");
+            scout = Content.Load<Texture2D>("scout");
             bg = temp;
+
+
+            btnEasy = new Button(easyButton, new Vector2(200, 300));
+            btnNormal = new Button(normalButton, new Vector2(500, 300));
+            btnHard = new Button(hardButton, new Vector2(800, 300));
         }
 
         protected override void Update(GameTime gameTime)
         {
-            this.Window.Title = mouseState.Position.ToString();
-            mouseState = Mouse.GetState();
             prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
+
+            // Update window title with the FRESH mouse position
+            this.Window.Title = mouseState.Position.ToString();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
             if (screen == Screen.Title)
             {
                 if (mouseState.LeftButton == ButtonState.Pressed && playRec.Contains(mouseState.Position))
                 {
                     screen = Screen.Play;
-
-                    
-
                 }
-
-
-
-
-
-
             }
-            else if (screen == Screen.Play)
+            else if (screen == Screen.Play) 
             {
-                int scaledWidth = (int)(easyButton.Width * scale);
-                int scaledHeight = (int)(easyButton.Height * scale);
-                Rectangle targetRect = new Rectangle((int)(easyButton. - (scaledWidth / 2f)), (int)(easyButton.Y - (scaledHeight / 2f)), scaledWidth, scaledHeight );
-                
-                // Check if mouse cursor is inside the rectangle
-                if (targetRect.Contains(mouseState.X, mouseState.Y))
+                btnEasy.Update(mouseState, prevMouseState);
+                btnNormal.Update(mouseState, prevMouseState);
+                btnHard.Update(mouseState, prevMouseState);
+                if (mouseState.LeftButton == ButtonState.Pressed && inventoryRec.Contains(mouseState.Position))
                 {
-                    // Smoothly scale up
-                    scale = MathHelper.Lerp(scale, HoverScale, LerpSpeed);
-                }
-                else
-                {
-                    // Smoothly scale down
-                    scale = MathHelper.Lerp(scale, NormalScale, LerpSpeed);
+                    screen = Screen.TowerPick;
                 }
 
+                if (btnEasy.IsClicked) screen = Screen.Easy;
+                if (btnNormal.IsClicked) screen = Screen.Normal;
+                if (btnHard.IsClicked) screen = Screen.Hard;
+            }
+            else if (screen == Screen.TowerPick)
+            {
+                bg = temp;
 
-                    if (mouseState.LeftButton == ButtonState.Pressed && easyRec.Contains(mouseState.Position))
-                {
-                    screen = Screen.Easy;
-                }
-                if (mouseState.LeftButton == ButtonState.Pressed && normalRec.Contains(mouseState.Position))
-                {
-                    screen = Screen.Normal;
-                }
-                if (mouseState.LeftButton == ButtonState.Pressed && hardRec.Contains(mouseState.Position))
-                {
-                    screen = Screen.Hard;
-                }
             }
             else if (screen == Screen.Easy)
             {
@@ -159,15 +153,21 @@ namespace Final_Project_or_smth_idk_teach
             }
             else if (screen == Screen.Play)
             {
+                Vector2 easyOrigin = new Vector2(easyButton.Width / 2f, easyButton.Height / 2f);
                 _spriteBatch.Draw(bg, window, Color.White);
-                _spriteBatch.Draw(easyButton, position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None,0f);
-                _spriteBatch.Draw(normalButton, normalRec, Color.White);
-                _spriteBatch.Draw(hardButton, hardRec, Color.White);
-
+                 btnEasy.Draw(_spriteBatch);
+    btnNormal.Draw(_spriteBatch);
+    btnHard.Draw(_spriteBatch);
+                _spriteBatch.Draw(inventory, inventoryRec, Color.White);
                 //REMEMBER TO MAKE THE DIFFUICULTY BUTTONS GET BIGGER WHEN HOVERED OVER YOU BUM
-                
 
 
+
+
+            }
+            else if (screen == Screen.TowerPick)
+            {
+                _spriteBatch.Draw(bg, window, Color.White);
 
             }
             else if (screen == Screen.Easy)
@@ -209,5 +209,67 @@ namespace Final_Project_or_smth_idk_teach
 
             base.Draw(gameTime);
         }
+
+        // This was way to complicated for little ol me and prob unoptimised but it lowkey worked so idc
+        public class Button
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+        private float _scale;
+        
+        private const float NormalScale = 0.4f;
+        private const float HoverScale = 0.6f;
+        private const float LerpSpeed = 0.15f;
+
+        // Public properties to access state from Game1
+        public Rectangle Hitbox { get; private set; }
+        public bool IsClicked { get; private set; }
+
+        public Button(Texture2D texture, Vector2 position)
+        {
+            _texture = texture;
+            _position = position;
+            _scale = NormalScale;
+        }
+
+        public void Update(MouseState mouseState, MouseState prevMouseState)
+        {
+            
+            int scaledWidth = (int)(_texture.Width * _scale);
+            int scaledHeight = (int)(_texture.Height * _scale);
+
+            
+            Hitbox = new Rectangle(
+                (int)_position.X - (scaledWidth / 2),
+                (int)_position.Y - (scaledHeight / 2),
+                scaledWidth,
+                scaledHeight
+            );
+
+            // 3. Hover scale interpolation
+            if (Hitbox.Contains(mouseState.Position))
+            {
+                _scale = MathHelper.Lerp(_scale, HoverScale, LerpSpeed);
+            }
+            else
+            {
+                _scale = MathHelper.Lerp(_scale, NormalScale, LerpSpeed);
+            }
+
+           
+            IsClicked = Hitbox.Contains(mouseState.Position) && 
+                        mouseState.LeftButton == ButtonState.Pressed && 
+                        prevMouseState.LeftButton == ButtonState.Released;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            
+            Vector2 origin = new Vector2(_texture.Width / 2f, _texture.Height / 2f);
+            
+            spriteBatch.Draw(_texture, _position, null, Color.White, 0f, origin, _scale, SpriteEffects.None, 0f);
+        }
     }
 }
+    }
+
